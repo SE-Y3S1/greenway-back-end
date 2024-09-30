72d40ccf7139f16ee39534be2c8ec8635788658f
KavindyaDolawatta <158482698+KavindyaDolawatta@users.noreply.github.com>
2024-09-30 07:27:39 -0700

    new

--- src/api/schedule.js
+++ src/api/schedule.js
@@ -1,1 +1,37 @@
-console.log("schedules")
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const scheduleSchema = new Schema({
+        name : {
+            type: String,
+            required: true
+        },
+        date : {
+            type: Date,
+            required: true
+        },
+        Time : {
+            type: String,
+            required: true
+        },
+        GarbageType : {
+            type: String,
+            required: true
+        },
+        collectionZone : {
+            type: String,
+            required: true
+        },
+        AssignedVehicle : {
+            type: String,
+            required: true
+        },
+        frequancy : {
+            type: String,
+            required: true
+        }
+})
+
+const Schedule = mongoose.model("Schedule",scheduleSchema);
+
+module.exports = Schedule;
